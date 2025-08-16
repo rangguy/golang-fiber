be2eb35b2c6e1a5bd1fff7a1d24136049e4dbb80
rangguy <ranggadwi100@gmail.com>
2025-08-16 18:36:37 +0700

    HTTP Request

--- fiber_test.go
+++ fiber_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -48,4 +49,23 @@ func TestCtx(t *testing.T) {
 	bytes, err = io.ReadAll(resp.Body)
 	assert.Nil(t, err, "Error should be nil")
 	assert.Equal(t, "Hello Guest", string(bytes))
+}
+
+func TestHttpRequest(t *testing.T) {
+	app.Get("/request", func(c *fiber.Ctx) error {
+		first := c.Get("firstname")   // header
+		last := c.Cookies("lastname") // cookies
+		return c.SendString("Hello " + first + " " + last)
+	})
+
+	request := httptest.NewRequest("GET", "/request", nil)
+	request.Header.Set("firstname", "Rangga")
+	request.AddCookie(&http.Cookie{Name: "lastname", Value: "Mahendra"})
+	resp, err := app.Test(request)
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+
+	bytes, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, "Hello Rangga Mahendra", string(bytes))
 }
