fe3037db17f6661edfeadcc8c236b0901fb0d9ab
rangguy <ranggadwi100@gmail.com>
2025-08-16 12:59:04 +0700

    ctx

--- fiber_test.go
+++ fiber_test.go
@@ -8,8 +8,9 @@ import (
 	"testing"
 )
 
+var app = fiber.New()
+
 func TestRoutingHelloWorld(t *testing.T) {
-	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
 	})
@@ -22,4 +23,29 @@ func TestRoutingHelloWorld(t *testing.T) {
 	bytes, err := io.ReadAll(resp.Body)
 	assert.Nil(t, err, "Error should be nil")
 	assert.Equal(t, "Hello World", string(bytes))
+}
+
+func TestCtx(t *testing.T) {
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		name := c.Query("name", "Guest")
+		return c.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest("GET", "/hello?name=Rangga", nil)
+	resp, err := app.Test(request)
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+
+	bytes, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, "Hello Rangga", string(bytes))
+
+	request = httptest.NewRequest("GET", "/hello", nil)
+	resp, err = app.Test(request)
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+
+	bytes, err = io.ReadAll(resp.Body)
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, "Hello Guest", string(bytes))
 }
